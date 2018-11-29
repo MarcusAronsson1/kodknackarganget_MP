4afd9a64ca46cb4ffd15e792c86f00ce5c8073ab
MarcusAronsson1 <marcus.dag.aronsson@gmail.com>
2018-11-29 10:27:07 +0100

    changed package structure

--- src/com/kodknackarganget/mp/Main.java
+++ src/com/kodknackarganget/mp/Main.java
@@ -1,19 +1,31 @@
-package sample;
+package com.kodknackarganget.mp;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        final Button button = new Button();
+        button.setText("Haj du");
+        button.setOnAction((ActionEvent event) -> {
+            // Printing Hello World! to the console
+            System.out.println("Hello World!");
+        });
+
         primaryStage.setTitle("Hello kodknäckargänget!");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(button, 300, 275));
         primaryStage.show();
+
     }
 
 
---
+++ src/com/kodknackarganget/mp/Project.java
@@ -0,0 +1,4 @@
+package com.kodknackarganget.mp;
+
+public class Project {
+}
