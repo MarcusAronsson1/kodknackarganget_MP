b62f0d71e1a42dac4d3b16032f35ad71554d18f2
Reagaz <Reagaz@users.noreply.github.com>
2018-11-29 14:15:13 +0100

    Added attributes to task class

--- src/com/kodknackarganget/mp/Task.java
+++ src/com/kodknackarganget/mp/Task.java
@@ -1,5 +1,14 @@
 package com.kodknackarganget.mp;
 
+import java.util.ArrayList;
+
 public class Task {
-    
+    private Arraylist<Member> taskMembers;
+    private Schedule schedule;
+    private ArrayList<Risk> taskRisks;
+    private double moneyBudget;
+    private String description;
+    private boolean isCompleted;
+
+    public Task()
 }
