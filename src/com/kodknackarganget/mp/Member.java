11e74f2fcf8142c70207adcd24542e7b4a95b33b
Fredrik Sjoegren <fredriksjgrn@gmail.com>
2018-12-01 12:34:04 +0100

    Some changes to the member class, encapsulation fixed and one method working but not to its fullest potential

--- src/com/kodknackarganget/mp/Member.java
+++ src/com/kodknackarganget/mp/Member.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Member {
-    String name;
-    int id;
-    String email;
-    double costPerHour;
-    ArrayList<String> taskNames;
-    HashMap<String, Integer> hoursWorked;
+   private String name;
+   private int id;
+   private String email;
+   private double costPerHour;
+   private ArrayList<String> taskNames;
+   private HashMap<String, Double> hoursWorked;
 
     public Member(String name, int id, String email, double costPerHour){
         this.name = name;
@@ -24,9 +24,14 @@ public class Member {
         //Should forward something from Task, I would assume :)
     }
 
-    public void addHoursWorked(String description, double hours){
-        getHoursWorked()
-        hoursWorked // Add something to it i guess? not really sure
+    public void addHoursWorked(String description, double hours) throws Exception{ // addHoursWorked on a specific task for a member
+            if ((hoursWorked.containsKey(description))){
+               hoursWorked.put(description,hours); // not sure if this is correct (Right now it just replaces the old hours and description)
+            }else{
+             Exception wrongKey = new Exception("The task does not exist, try again");
+                  throw wrongKey;         // Add an error, since the description is not matched to an task
+            }
+
     }
 
 
@@ -50,7 +55,11 @@ public class Member {
         return taskNames;
     }
 
-    public HashMap<String, Integer> getHoursWorked() {
+    public HashMap<String, Double> getHoursWorked() {
         return hoursWorked;
     }
+    public double getHoursWorked2(){
+        return hoursWorked.get();
+    }
+
 }
