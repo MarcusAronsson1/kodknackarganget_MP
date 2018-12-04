f75d337d0107b0722e0488fe7b4295cba4190fbf
Fredrik Sjögren <Fredrik@Fredriks-MacBook-Pro.local>
2018-12-04 13:00:54 +0100

    Changes to the HashMap, it is a double now, might be changed later on.

--- src/com/kodknackarganget/mp/Member.java
+++ src/com/kodknackarganget/mp/Member.java
@@ -8,30 +8,26 @@ public class Member {
    private int id;
    private String email;
    private double costPerHour;
+   private double hoursWorked;
    private ArrayList<String> taskNames;
-   private HashMap<String, Double> hoursWorked;
+
+  // private HashMap<String, Double> hoursWorked;
 
     public Member(String name, int id, String email, double costPerHour){
         this.name = name;
         this.id = id;
         this.email = email;
         this.costPerHour = costPerHour;
-        this.hoursWorked = new HashMap<>();
+        this.hoursWorked = 0;
         this.taskNames = new ArrayList<>();
     }
 
     public void addTask(String description){
-        //Should forward something from Task, I would assume :)
+        
     }
 
-    public void addHoursWorked(String description, double hours) throws Exception{ // addHoursWorked on a specific task for a member
-            if ((hoursWorked.containsKey(description))){
-               hoursWorked.put(description,hours); // not sure if this is correct (Right now it just replaces the old hours and description)
-            }else{
-             Exception wrongKey = new Exception("The task does not exist, try again");
-                  throw wrongKey;         // Add an error, since the description is not matched to an task
-            }
-
+    public void addHoursWorked(double hours){ // addHoursWorked for a member
+        hoursWorked += hours;
     }
 
 
@@ -55,11 +51,8 @@ public class Member {
         return taskNames;
     }
 
-    public HashMap<String, Double> getHoursWorked() {
+    public double getHoursWorked() {
         return hoursWorked;
-    }
-    public double getHoursWorked2(){
-        return hoursWorked.get();
     }
 
 }
