3586727b16ec2cb9118bb358229684f5c1be2d34
august axelsson <augustaxelsson1@augusts-MacBook-Pro-2.local>
2018-12-20 11:17:58 +0100

    Added all of my code

---
+++ src/com/kodknackarganget/mp/MainStage/code/Main.java
@@ -0,0 +1,33 @@
+package sample;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.paint.Color;
+
+
+public class Main extends Application {
+
+    public static Stage stage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../FXML/main.fxml"));
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.getScene().setFill(Color.TRANSPARENT);
+        primaryStage.setOpacity(1);
+        primaryStage.show();
+        stage = primaryStage;
+
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
