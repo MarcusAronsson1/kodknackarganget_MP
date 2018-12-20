0a1ca2ee1099f55eb6448a98c343c511318c1b7d
august axelsson <augustaxelsson1@augusts-MacBook-Pro-2.local>
2018-12-20 17:13:24 +0100

    added more functions

--- src/com/kodknackarganget/mp/MainStage/code/FxmlMainController.java
+++ src/com/kodknackarganget/mp/MainStage/code/FxmlMainController.java
@@ -1,103 +1,93 @@
-package sample;
+package com.kodknackarganget.mp.MainStage.code;
+
+import static com.kodknackarganget.mp.MainStage.code.Main.stage;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-
 import java.io.IOException;
 
-import static sample.Main.stage;
 
 public class FxmlMainController {
 
-        @FXML
-        private Button exitBtn;
-        @FXML
-        private Button memberBtn;
-        @FXML
-        private Button taskBtn;
-        @FXML
-        private Button homeBtn;
-        @FXML
-        private Button searchBtn;
-        @FXML
-        private Pane changePane;
-        @FXML
-        private AnchorPane anchorPane;
-        @FXML
-        private ProgressBar prog;
-        @FXML
-        private Button but;
+    @FXML
+    private Button exitBtn;
+    @FXML
+    private Button memberBtn;
+    @FXML
+    private Button taskBtn;
+    @FXML
+    private Button homeBtn;
+    @FXML
+    private Button searchBtn;
+    @FXML
+    private Pane changePane;
+    @FXML
+    private AnchorPane anchorPane;
+    @FXML
+    private ProgressBar prog;
+    @FXML
+    private Button but;
+    @FXML
+    private Button saveBtn;
 
-        public void setProg(double val){
-            prog.setProgress(val);
-        }
+    public void setProg(double val) {
+        prog.setProgress(val);
+
+    }
 
 
     public void initialize() {
 
-        but.setOnAction(e ->{
-            setProg(0.9);
+        but.setOnAction(e -> {
+            setProg(prog.getProgress() + 0.1);
         });
 
+        exitBtn.setOnAction(e -> stage.close());
 
-            exitBtn.setOnAction(e -> stage.close());
 
+        homeBtn.setOnAction(e -> {
+            try {
+                anchorPane.getChildren().clear();
+                Parent home = FXMLLoader.load(getClass().getResource("../FXML/main.fxml"));
+                anchorPane.getChildren().add(home);
 
-            homeBtn.setOnAction(e -> {
-
-                try {
-                    anchorPane.getChildren().clear();
-                    Parent home = FXMLLoader.load(getClass().getResource("../FXML/main.fxml"));
-                    anchorPane.getChildren().add(home);
-                    
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                }
-            });
-
-            memberBtn.setOnAction(e -> {
-                try {
-                    changePane.getChildren().clear();
-                    Parent member = FXMLLoader.load(getClass().getResource("../FXML/member.fxml"));
-                    changePane.getChildren().add(member);
-
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                }
-            });
-
-            taskBtn.setOnAction(e -> {
-
-                try {
-                        changePane.getChildren().clear();
-                        Parent task = FXMLLoader.load(getClass().getResource("../FXML/tasks.fxml"));
-                        changePane.getChildren().add(task);
-
-                }
-                catch (IOException e1) {
-                    e1.printStackTrace();
-                }
-
-            });
-
-            searchBtn.setOnAction(e -> {
-
-                try {
-                    changePane.getChildren().clear();
-                    Parent search = FXMLLoader.load(getClass().getResource("../FXML/search.fxml"));
-                    changePane.getChildren().add(search);
-                }
-                catch (IOException e1) {
-                    e1.printStackTrace();
-                }
-
-            });
-
+            } catch (IOException e1) {
+                e1.printStackTrace();
             }
+        });
+
+        memberBtn.setOnAction(e -> {
+            try {
+                changePane.getChildren().clear();
+                Parent member = FXMLLoader.load(getClass().getResource("../FXML/member.fxml"));
+                changePane.getChildren().add(member);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
+
+        taskBtn.setOnAction(e -> {
+            try {
+                changePane.getChildren().clear();
+                Parent task = FXMLLoader.load(getClass().getResource("../FXML/tasks.fxml"));
+                changePane.getChildren().add(task);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
+
+        searchBtn.setOnAction(e -> {
+            try {
+                changePane.getChildren().clear();
+                Parent search = FXMLLoader.load(getClass().getResource("../FXML/search.fxml"));
+                changePane.getChildren().add(search);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
 
     }
+}
--- src/com/kodknackarganget/mp/MainStage/code/FxmlTasksController.java
+++ src/com/kodknackarganget/mp/MainStage/code/FxmlTasksController.java
@@ -1,4 +1,4 @@
-package sample;
+package com.kodknackarganget.mp.MainStage.code;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -6,6 +6,7 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import java.io.IOException;
 
@@ -13,172 +14,80 @@ import java.io.IOException;
 public class FxmlTasksController {
 
     @FXML
-    private CheckBox checkbox1, checkbox2, checkbox3, checkbox4, checkbox5, checkbox6;
-    @FXML
-    private CheckBox checkbox7, checkbox8, checkbox9, checkbox10, checkbox11, checkbox12;
-    @FXML
-    private Button addBtn1, addBtn2, addBtn3, addBtn4, addBtn5, addBtn6;
-    @FXML
-    private Button addBtn7, addBtn8, addBtn9, addBtn10, addBtn11, addBtn12;
-    //@FXML
-    //private Pane taskpane1;
-    @FXML
     private Pane addPane;
     @FXML
     private TextField addMemberTask;
     @FXML
     private Button doneBtn;
+    @FXML
+    private ImageView testaug;
 
 
+    public void checkboxRight(ActionEvent actionEvent) {
 
-    public void checkboxLeft(CheckBox box){
-        if (box.isSelected()) {
-            box.setOpacity(0.5);
-            Node test = box.getParent().getChildrenUnmodifiable().get(2);
-            test.setOpacity(0.5);
-            //getTask(test.getName()).isCompleted(true);
-        }else{
-            box.setOpacity(1);
-            Node test = box.getParent().getChildrenUnmodifiable().get(2);
-            test.setOpacity(1);
-        }
-    }
-
-    public void checkboxRight(CheckBox box){
+        CheckBox box = (CheckBox) actionEvent.getSource();
+        box.setId(box.getId());
         if (box.isSelected()) {
             box.setOpacity(0.5);
             Node test = box.getParent().getChildrenUnmodifiable().get(0);
             test.setOpacity(0.5);
             //getTask(taskpane1.getName()).isCompleted(true);
-        }else{
+        } else {
             box.setOpacity(1);
             Node test = box.getParent().getChildrenUnmodifiable().get(0);
             test.setOpacity(1);
         }
     }
+
+    public void checkboxLeft(ActionEvent actionEvent) {
+
+        CheckBox box = (CheckBox) actionEvent.getSource();
+        box.setId(box.getId());
+        if (box.isSelected()) {
+            box.setOpacity(0.5);
+            Node test = box.getParent().getChildrenUnmodifiable().get(2);
+            test.setOpacity(0.5);
+            //getTask(taskpane1.getName()).isCompleted(true);
+        } else {
+            box.setOpacity(1);
+            Node test = box.getParent().getChildrenUnmodifiable().get(2);
+            test.setOpacity(1);
+        }
+    }
+
+    public void addMemberToTask(ActionEvent actionEvent) {
+        Button button = (Button) actionEvent.getSource();
+        addPane.setOpacity(1);
+    }
+
 
     public void initialize() {
 
         doneBtn.setOnAction(e -> {
             addPane.setOpacity(0);
         });
-
-        checkbox1.setOnAction(e -> {
-            checkboxRight(checkbox1);
-        });
-
-        checkbox2.setOnAction(e -> {
-            checkboxLeft(checkbox2);
-        });
-
-        checkbox3.setOnAction(e -> {
-            checkboxRight(checkbox3);
-        });
-
-        checkbox4.setOnAction(e -> {
-            checkboxLeft(checkbox4);
-        });
-
-        checkbox5.setOnAction(e -> {
-            checkboxRight(checkbox5);
-        });
-
-        checkbox6.setOnAction(e -> {
-            checkboxLeft(checkbox6);
-        });
-
-        checkbox7.setOnAction(e -> {
-            checkboxRight(checkbox7);
-        });
-
-        checkbox8.setOnAction(e -> {
-            checkboxLeft(checkbox8);
-        });
-
-        checkbox9.setOnAction(e -> {
-            checkboxRight(checkbox9);
-        });
-
-        checkbox10.setOnAction(e -> {
-            checkboxLeft(checkbox10);
-        });
-
-        checkbox11.setOnAction(e -> {
-            checkboxRight(checkbox11);
-        });
-
-        checkbox12.setOnAction(e -> {
-            checkboxLeft(checkbox12);
-        });
-
-        addBtn1.setOnAction(e -> {
-
-            addPane.setOpacity(1);
-        });
-
-        addBtn2.setOnAction(e -> {
-
-            addPane.setOpacity(1);
-        });
-
-        addBtn3.setOnAction(e -> {
-
-            addPane.setOpacity(1);
-        });
-
-        addBtn4.setOnAction(e -> {
-
-            addPane.setOpacity(1);
-        });
-
-        addBtn5.setOnAction(e -> {
-
-            addPane.setOpacity(1);
-        });
-
-        addBtn6.setOnAction(e -> {
-
-            addPane.setOpacity(1);
-        });
-
-        addBtn7.setOnAction(e -> {
-
-            addPane.setOpacity(1);
-        });
-
-        addBtn8.setOnAction(e -> {
-
-            addPane.setOpacity(1);
-        });
-
-        addBtn9.setOnAction(e -> {
-
-            addPane.setOpacity(1);
-        });
-
-        addBtn10.setOnAction(e -> {
-
-            addPane.setOpacity(1);
-        });
-
-        addBtn11.setOnAction(e -> {
-
-            addPane.setOpacity(1);
-        });
-
-        addBtn12.setOnAction(e -> {
-
-            addPane.setOpacity(1);
-        });
-
     }
 
 
     public void onEnter(ActionEvent actionEvent) throws IOException {
 
-        doneBtn.setOpacity(1);
-       // String ID = addMemberTask.getText();
-        //task.addMember(ID);
-        addMemberTask.clear();
+        if (addMemberTask.getText().equals("1337")) {
+            //testaug.setOpacity(1);
+            doneBtn.setOpacity(1);
+            Node imageView = testaug.getParent().getChildrenUnmodifiable().get(3);
+            imageView.setOpacity(1);
+            // String ID = addMemberTask.getText();
+            //task.addMember(ID);
+            addMemberTask.clear();
+        } else {
+            doneBtn.setOpacity(1);
+            // String ID = addMemberTask.getText();
+            //task.addMember(ID);
+            addMemberTask.clear();
+            Node imageView = testaug.getParent().getChildrenUnmodifiable().get(3);
+            imageView.setOpacity(1);
+        }
     }
+
+
 }
